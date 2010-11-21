b0cee636ab2b2a949097f1c67b7ddf448daa61ad
Paweł Wrzosek <pawelwrzosek@gmail.com>
2010-11-21 20:56:18 +0000

    Ostatni...
    git-svn-id: svn://svn.code.sf.net/p/centixx/code@28 5aeba2df-c3bd-4be1-9e53-8833f76647b0

---
+++ centimm/library/Centixx/Form/AddItem.php
@@ -0,0 +1,36 @@
+<?php
+class Centixx_Form_AddItem extends Zend_Form
+{
+	protected $_items;
+	public $itemLabel = '';
+	public $submitLabel = 'Przypisz';
+
+	public function rebuild()
+	{
+		$this->setMethod(self::METHOD_POST);
+
+		$this->addElement('select', 'new_item', array(
+			'label'		=> $this->itemLabel,
+			'required'	=> true,
+			'multiOptions' => $this->_items,
+		));
+
+		$this->addelement('submit', 'submit', array(
+			'label'		=> $this->submitLabel,
+			'ignore'	=> true,
+		));
+	}
+
+	public function setValues($array) {
+		if (array_key_exists('items', $array)) {
+			$this->_items = $array['items'];
+		}
+		$this->rebuild();
+		return $this;
+	}
+
+	public function hasItems() {
+		debug($this->_items);
+		return count($this->_items) !== 0;
+	}
+}
