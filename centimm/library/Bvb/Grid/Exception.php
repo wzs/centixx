fdfee5f9c266e0b05965b701794e57381d789345
Kamil Zień <zien.kamil1@gmail.com>
2010-12-20 17:10:29 +0000

    od wrzoska
    git-svn-id: svn://svn.code.sf.net/p/centixx/code@58 5aeba2df-c3bd-4be1-9e53-8833f76647b0

---
+++ centimm/library/Bvb/Grid/Exception.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license
+ * It is  available through the world-wide-web at this URL:
+ * http://www.petala-azul.com/bsd.txt
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to geral@petala-azul.com so we can send you a copy immediately.
+ *
+ * @package    Bvb_Grid
+ * @copyright  Copyright (c)  (http://www.petala-azul.com)
+ * @license    http://www.petala-azul.com/bsd.txt   New BSD License
+ * @version    $Id: Exception.php 1180 2010-05-21 14:53:17Z bento.vilas.boas@gmail.com $
+ * @author     Bento Vilas Boas <geral@petala-azul.com >
+ */
+
+class Bvb_Grid_Exception extends Zend_Exception
+{
+}
---
+++ centimm/library/Bvb/Grid/Filters.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license
+ * It is  available through the world-wide-web at this URL:
+ * http://www.petala-azul.com/bsd.txt
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to geral@petala-azul.com so we can send you a copy immediately.
+ *
+ * @package    Bvb_Grid
+ * @copyright  Copyright (c)  (http://www.petala-azul.com)
+ * @license    http://www.petala-azul.com/bsd.txt   New BSD License
+ * @version    $Id: Filters.php 1168 2010-05-19 14:38:50Z bento.vilas.boas@gmail.com $
+ * @author     Bento Vilas Boas <geral@petala-azul.com >
+ */
+
+
+class Bvb_Grid_Filters
+{
+
+    public $_filters = array();
+
+
+    public function addFilter ($field, $options = array())
+    {
+        $this->_filters[$field] = $options;
+        return $this;
+    }
+
+}
