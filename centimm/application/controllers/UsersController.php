d5c15e4121467518eb02e17fbd7fb15638cf19c5
Paweł Wrzosek <pawelwrzosek@gmail.com>
2010-12-21 15:29:32 +0000

    Sprawdzanie, czy nie jest usuwany ostatni pracownik  HR/CEO
    git-svn-id: svn://svn.code.sf.net/p/centixx/code@77 5aeba2df-c3bd-4be1-9e53-8833f76647b0

--- centimm/application/controllers/UsersController.php
+++ centimm/application/controllers/UsersController.php
@@ -1,5 +1,7 @@
 <?php
 
+class RemoveLastRoleException extends Exception {}
+
 class UsersController extends Centixx_Controller_Action
 {
 	public function indexAction()
@@ -65,7 +67,7 @@ class UsersController extends Centixx_Controller_Action
 	}
 
 	/**
-	 * 
+	 *
 	 * Enter description here ...
 	 * @param Centixx_Model_User $user
 	 * @param string  $type
@@ -82,7 +84,7 @@ class UsersController extends Centixx_Controller_Action
 		$form = new Application_Form_User_Edit();
 
 		$roles = Centixx_Model_Mapper_Abstract::factory('Role')->fetchAll();
-		
+
 		$departments = Centixx_Model_Mapper_Abstract::factory('Department')->fetchAll();
 		$form->setValues(array(
 			'roles' 		=> $roles,
@@ -104,38 +106,47 @@ class UsersController extends Centixx_Controller_Action
 
 				//README: wykomentowany nizej warunek pozwalal na edycje CEO bez sesji,
 				//ale tylko kiedy nie zmieniano jego poziomu uprawnien. Teraz ograniczenie jest na jakakolwiek edycje
-				
-//				//aby zmienić uprawnienia użytkownika na CEO / zmniejszyć uprawnienia CEO
-//				//obecnie zalogowany user MUSI mieć nadane pozwolenie
-//				if (
-//					($user->hasRole(Centixx_Acl::ROLE_CEO) && !in_array(Centixx_Acl::ROLE_CEO, $data['roles'])) ||
-//					(!$user->hasRole(Centixx_Acl::ROLE_CEO) && in_array(Centixx_Acl::ROLE_CEO, $data['roles']))
-//				) {
+
+				//				//aby zmienić uprawnienia użytkownika na CEO / zmniejszyć uprawnienia CEO
+				//				//obecnie zalogowany user MUSI mieć nadane pozwolenie
+				//				if (
+				//					($user->hasRole(Centixx_Acl::ROLE_CEO) && !in_array(Centixx_Acl::ROLE_CEO, $data['roles'])) ||
+				//					(!$user->hasRole(Centixx_Acl::ROLE_CEO) && in_array(Centixx_Acl::ROLE_CEO, $data['roles']))
+				//				) {
 
 				if ($user->hasRole(Centixx_Acl::ROLE_CEO)) {
-				
+
 					if (!$user->isAllowed($this->_currentUser, Centixx_Model_User::ACTION_ADD_CEO)) {
 						throw new Centixx_Acl_AuthenticationException("Nie masz uprawnień do edycji członka zarządu");
 					}
 					$this->_currentUser->removePermission(Centixx_Model_User::ACTION_ADD_CEO);
 				}
 
-				$user->setOptions($data)->save();
+				try {
+					//jesli zmniejsza się uprawnienie z CEO / kadrowych, to sprawdzamy, czy nie bedzie usuniety ostatni z nuch
+					$this->checkIsLastRole(Centixx_Acl::ROLE_CEO, $user, $data);
+					$this->checkIsLastRole(Centixx_Acl::ROLE_HR, $user, $data);
 
-				if ($data['department']) {
-					$department = Centixx_Model_Mapper_Abstract::factory('Department')->find($data['department']);
-					$department->setManager($user)->save();
+
+					$user->setOptions($data)->save();
+
+					if ($data['department']) {
+						$department = Centixx_Model_Mapper_Abstract::factory('Department')->find($data['department']);
+						$department->setManager($user)->save();
+					}
+
+					if ($type == 'add') {
+						$this->log(Centixx_Log::USER_CREATED, $user);
+						$this->addFlashMessage('Użytkownik został dodany', false, true);
+					} else {
+						$this->addFlashMessage('Dane zostały zaktualizowane', false, true);
+						$this->log(Centixx_Log::USER_UPDATED, $user);
+					}
+				} catch (RemoveLastRoleException $e) {
+					$this->addFlashMessage($e->getMessage(), true, true);
 				}
 
-				if ($type == 'add') {
-					$this->log(Centixx_Log::USER_CREATED, $user);
-					$this->addFlashMessage('Użytkownik został dodany', false, true);
-				} else {
-					$this->addFlashMessage('Dane zostały zaktualizowane', false, true);
-					$this->log(Centixx_Log::USER_UPDATED, $user);
-				}
 				$this->_redirect($this->redirect('users'));
-
 
 			} else {
 				$this->addFlashMessage('Formularz wypełniony niepoprawnie', true, true);
@@ -152,4 +163,25 @@ class UsersController extends Centixx_Controller_Action
 
 		$this->view->user = $user;
 	}
+
+
+	/**
+	 * Sprawdza, czy nie jest odbierane ustawienie dla ostatniego użytkownika z daną rolą
+	 *
+	 * @param int $role id roli
+	 * @param Centixx_Model_User $user
+	 * @param array $data
+	 * @throws RemoveLastRoleException
+	 */
+	protected function checkIsLastRole($role, $user, $data)
+	{
+		if ($user->hasRole($role) && !in_array($role, $data['roles'])) {
+			//ilosc CEO
+			$count = $this->_db->fetchCol("SELECT COUNT(*) FROM users_roles WHERE role_id = ?", array($role));
+			if ($count[0] == 1) {
+				throw new RemoveLastRoleException('Nie możesz zmniejszyć uprawnień temu użytkownikowi, gdyż jest to ostatnia osoba pełniąca daną rolę');
+			}
+		}
+	}
+
 }
